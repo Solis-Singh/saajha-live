7ca5f38bb20a3018952ab18faf968c9c29715ef2
Suraj Prakash <solissingh@gmail.com>
2025-06-21 20:38:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: 'https://saajha-frontend.onrender.com' }));
+app.use(cors({ origin: 'https://saajhalive.vercel.app/' }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
