1c8f8e38150eb35be58f8097226bd807c6f43bf4
Suraj Prakash <solissingh@gmail.com>
2025-06-21 20:26:38 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://saajha-backend-0xvm.onrender.com/api';
+const API_URL = 'http://localhost:5000/api';
 
 // Create axios instance with default config
 const api = axios.create({
